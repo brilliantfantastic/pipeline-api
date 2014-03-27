3c1bf4b121e4025a356cd0ad85d50ebf3a57cc06
Jamie Wright <jamie@brilliantfantastic.com>
2014-03-27 14:35:26 -0400

    Period#week must be present

--- spec/models/period_spec.rb
+++ spec/models/period_spec.rb
@@ -7,6 +7,13 @@ describe Period do
       Period.create week: week
       period = Period.new week: week
       period.wont_be :valid?
+      period.errors[:week].must_include "has already been taken"
+    end
+
+    it 'requires a week' do
+      period = Period.new
+      period.wont_be :valid?
+      period.errors[:week].must_include "can't be blank"
     end
   end
 end
